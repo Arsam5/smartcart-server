e06e30f07a8cbfbb6c7f80842edb3997fac2eaf2
Arsam Kamran <bitf14a020@pucit.edu.pk>
2019-03-28 09:35:05 +0000

    updated code

--- app.py
+++ app.py
@@ -18,115 +18,43 @@ def hello():
 	name = request.args.get('name', 'World')
 	return ('<h1>Hello, {}!<h1>'.format(name))
 
-
-@app.route('/weather/<string:city>', methods=['GET'])
-def get_weather(city):
-	data = session.execute("""SELECT * FROM weather.city WHERE name = '{}' ALLOW FILTERING""".format(city))
-
-	weather_url = base_url.format(city = city, units = 'metric', API_KEY = API_KEY)
-
-	resp = requests.get(weather_url)
-
-	if resp.ok:
-		# print(resp.json())
-
-		print(data, file=sys.stderr)
-
-		res = resp.json()
-		weather = {
-			'id': data.id,
-			'city': city,
-			'temperature': res['main']['temp'],
-			'description': res['weather'][0]['description'],
-			'icon' : res['weather'][0]['icon']
-		}
-
-		return jsonify(weather), resp.status_code
-	else:
-		return resp.reason
-
-@app.route('/weather/<int:id>', methods=['GET'])
-def get_weather_by_id(id):
-
-	#data = [x for x in cities if x['id'] == id]
-	rows = session.execute("""SELECT * FROM weather.city WHERE id=%(id)s""",{'id': id})
-	data = None
-	for row in rows:
-		data = row
-	print(data, file=sys.stderr)
-	weather_url = base_url.format(city = data.original, units = 'metric', API_KEY = API_KEY)
-
-	resp = requests.get(weather_url)
-
-	if resp.ok:
-		# print(resp.json())
-		print(data)
-		res = resp.json()
-		weather = {
-			'id': id,
-			'city': data.name,
-			'original': data.original,
-			'temperature': res['main']['temp'],
-			'description': res['weather'][0]['description'],
-			'icon' : res['weather'][0]['icon']
-		}
-		print(weather)
-
-		return jsonify(weather), resp.status_code
-	else:
-		return resp.reason
-
-@app.route('/weather', methods=['GET'])
-def get_weather_for_all():
-	weather_data = []
-
-	cities = session.execute("SELECT * FROM weather.city")
-
-	print(cities)
-	for city in cities:
-		print(city)
-		weather_url = base_url.format(city = city.original, units = 'metric', API_KEY = API_KEY)
-		print('before call')
-		resp = requests.get(weather_url).json()
-
-		if resp['cod'] == '404':
-			continue
-		print('before weather')
-		weather = {
-			'id': city.id,
-			'name': city.name,
-			'original': city.original,
-			'temperature': resp['main']['temp'],
-			'description': resp['weather'][0]['description'],
-			'icon' : resp['weather'][0]['icon']
-		}
-		print('after weather')
-
-		weather_data.append(weather)
-
-
-	return jsonify(weather_data), 200
-
 #_________________________________________________________________________________________________________________________________________________________________
 #my post request for signup call
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup_user():
 	print('inside post')
-	resp = session.execute("INSERT INTO smartcart.user(id,name,email,password) VALUES(%s, %s, %s, %s)", (request.args['id'],.args['name'], request.args['email'], request.args['password']))
+	resp = session.execute("INSERT INTO smartcart.user(id,name,email,password) VALUES(%s, %s, %s, %s)", (int(request.args['id']),request.args['name'], request.args['email'], request.args['password']))
 	return jsonify({'message': '1'}), 201
 
+#login call
+@app.route('/login',methods =['POST'])
+def login_user():
+	rows = session.execute("""select name from smartcart.user where email =%(email)s ALLOW FILTERING""",{'email': request.args['email']})
+	data = None
+
+	for row in rows:
+		data = row
+		print(row)
+
+	if data:
+		return jsonify({'message': '1'.format(id)}), 200
+	else:
+		return resp.reason
+
 #get product from database
-@app.route('/product/<int:itemid>', methods=['GET'])
-def get_product_by_id(itemid):
+@app.route('/product/<int:itemId>', methods=['GET'])
+def get_product_by_id(itemId):
 
 	#data = [x for x in cities if x['id'] == id]
 	#comment
-	rows = session.execute("""SELECT * FROM smartcart.product WHERE itemid=%(itemid)s""",{'itemid': itemid})
+	rows = session.execute("""SELECT * FROM smartcart.products WHERE itemId=%(itemId)s""",{'itemId': itemId})
 	data = None
+
 	for row in rows:
 		data = row
+		print(row)
 
-	product_url = basee_url.format(itemid = data.itemid, API_KEY = API_KEY)
+	product_url = basee_url.format(itemId = itemId, API_KEY = API_KEY)
 
 	resp = requests.get(product_url)
 
@@ -135,7 +63,7 @@ def get_product_by_id(itemid):
 
 		res = resp.json()
 		item = {
-			'itemid': itemid,
+			'itemId': itemId,
 			'name': data.name,
 			'price': res['salePrice'],
 			'description': data.description
@@ -148,117 +76,61 @@ def get_product_by_id(itemid):
 
 
 #insert product into database
-@app.route('/item', methods= ['POST'])
+@app.route('/items', methods= ['POST'])
 def create_product():
-	print('inside create product post request')
 
-	product_url = basee_url.format(itemid = request.form['itemid'], API_KEY= API_KEY)
-	resp = request.post(product_url,data = {'itemid':request.form['itemid']}).json()
+	print ('fuck off')
+
+	product_url = basee_url.format(itemId = request.args['itemId'] , API_KEY= API_KEY)
+	print(product_url)
+	resp = request.post(product_url,data = {'itemId':request.args['itemId']}).json()
 	print('after product call')
-	count_rows = session.execute("SELECT COUNT(*) FROM smartcart.product")
+
+
+
+	count_rows = session.execute("SELECT COUNT(*) FROM smartcart.products")
 
 	for c in count_rows:
 		last_id = c.count
 	last_id += 1
-	print(last_id, file=sys.stderr)
-	resp = session.execute("INSERT INTO smartcart.product(id,itemid,name,price,description) VALUES(%s, %s, %s, %s, %s)", (last_id,request.form['itemid'], request.form['name'], resp['salePrice'], resp['shortDescription']))
-	print(resp, file=sys.stderr)
+
+	print(request.form['name'])
+
+	# print(request.args)
+	resp = session.execute("INSERT INTO smartcart.product(itemId,description,id,name,price) VALUES(%s, %s, %s, %s, %s)", (request.form['itemId'], request.form['shortDescription'], last_id,request.form['name'], request.form['salePrice']))
+
 	print('done')
 
 	return jsonify({'message': 'added'}), 201
 
 #delete product from database by itemid
-@app.route('/product/<int:id>', methods = ['DELETE'])
-def delete_product_by_id(id):
-	if not id:
-		return jsonify({'Error': 'The id is needed to delete'}), 400
+@app.route('/deleteproduct/<int:itemId>', methods = ['DELETE'])
+def delete_product_by_id(itemId):
+	# if not itemId:
+	# 	return jsonify({'Error': 'The id is needed to delete'}), 400
 	print('before delete')
-	resp = session.execute("""DELETE FROM smartcart.product WHERE id={}""".format(id))
-	print(resp)
-	print('after delete')
+	resp = session.execute("""DELETE FROM smartcart.products WHERE itemId={}""".format(itemId))
 
-	return jsonify({'message': 'deleted: /weather/{}'.format(id)}), 200
+	return jsonify({'message': 'deleted'.format(itemId)}), 200
 
 #edit product into database
-@app.route('/product/<int:id>', methods = ['PUT'])
-def update_product(id):
+@app.route('/editproduct/<int:itemId>', methods = ['PUT'])
+def update_product(itemId):
 
 	print('inside put')
 
-	if not request.form or not 'itemid' in request.form:
-		return jsonify({'Error': 'Record does not exist'}), 404
+	#if not request.args or not 'itemId' in request.args:
+	#	return jsonify({'Error': 'Record does not exist'}), 404
 
 	print('inside update')
-	rows = session.execute("""UPDATE smartcart.prodcut SET name=%(name)s WHERE id=%(id)s""", {'name': request.form['name'], 'id': id})
+	rows = session.execute("""UPDATE smartcart.products SET name=%(name)s WHERE itemId=%(itemId)s""", {'name': request.args['name'], 'itemId': itemId})
 
 	print('after update')
 
-	return jsonify({'message':'updated: /prodcut/{}'.format(id)}), 200
+	return jsonify({'message':'1'.format(id)}), 200
 
 
 #________________________________________________________________________________________________________________________________________________________________
-@app.route('/weather', methods = ['POST'])
-def create_city():
-
-	print('inside post')
-
-	if not request.form or not 'city' in request.form:
-		return jsonify({'Error': 'The new record needs to have a city name'}), 400
-
-	print('before weather call')
-	weather_url = base_url.format(city = request.form['city'], units = 'metric', API_KEY = API_KEY)
-	resp = requests.post(weather_url, data = {'city': request.form['city']} ).json()
-	print('after weather call')
-	count_rows = session.execute("SELECT COUNT(*) FROM weather.city")
-
-	#last_id = cities[-1]['id'];
-	for c in count_rows:
-		last_id = c.count
-	last_id += 1
-	print(last_id, file=sys.stderr)
-	resp = session.execute("INSERT INTO weather.city(id,name,original,temperature,description,icon) VALUES(%s, %s, %s, %s, %s, %s)", (last_id, request.form['city'], request.form['city'], resp['main']['temp'], resp['weather'][0]['description'],resp['weather'][0]['icon']))
-	print(resp, file=sys.stderr)
-	#cities.append({'id': last_id, 'name':request.form['city'], 'original':request.form['city']})
-	print('done')
-
-	return jsonify({'message': 'created new city with weather'}), 201
-
-@app.route('/weather/<int:id>', methods = ['PUT'])
-def update_city(id):
-
-	print('inside put')
-
-	if not request.form or not 'city' in request.form:
-		return jsonify({'Error': 'Record does not exist'}), 404
-
-	print('inside update')
-	rows = session.execute("""UPDATE weather.city SET name=%(name)s WHERE id=%(id)s""", {'name': request.form['city'], 'id': id})
-
-	print('after update')
-
-	return jsonify({'message':'updated: /weather/{}'.format(id)}), 200
-
-
-@app.route('/weather/<string:city>', methods = ['DELETE'])
-def delete_city(city):
-	if not city:
-		return jsonify({'Error': 'The city name is needed to delete'}), 400
-
-	session.execute("""DELETE FROM weather.city WHERE name='{}'""".format(city))
-
-	return jsonify({'message': 'deleted: /weather/{}'.format(city)}), 200
-
-@app.route('/weather/<int:id>', methods = ['DELETE'])
-def delete_city_by_id(id):
-	if not id:
-		return jsonify({'Error': 'The id is needed to delete'}), 400
-	print('before delete')
-	resp = session.execute("""DELETE FROM weather.city WHERE id={}""".format(id))
-	print(resp)
-	print('after delete')
-
-	return jsonify({'message': 'deleted: /weather/{}'.format(id)}), 200
-
 
 @app.route('/pokemon/<name>')
 def profile(name):
